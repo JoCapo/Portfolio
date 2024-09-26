2593c27c5d6711919c6a06fa970a0bf564b90eff
JoCapo <97632697+JoCapo@users.noreply.github.com>
2024-09-26 17:29:33 -0400

    nav bar on click change

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-let section = document.querySelectorAll("section");
-let menu = document.querySelectorAll("nav li a");
+let section = document.querySelectorAll(".section");
+//let menu = document.querySelectorAll("nav li a");
+
+var nav = document.querySelectorAll(".nav-link");
+
+function activeLink(li){
+  nav.forEach((item) =>
+  item.classList.remove('active'));
+  li.classList.add('active');
+}
+
+nav.forEach((item) =>
+  item.addEventListener('click',function(){
+    activeLink(this)
+  }
+));
+
+
+window.onscroll = () =>{
+  section.forEach(sec =>{
+    let top = window.scrollY;
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      const target = document.querySelector(`[href='#${id}']`).parentElement;
+                    activeLink(target);
+      // nav.forEach((item) => {
+      //     item.classList.remove('active');
+      //     if(item.querySelector('a').getAttribute('href') == '#' + id){
+      //         item.classList.add('active');
+      //     }
+      //  });            
+  }
+  })
+};
 
 window.onscroll = () => {
   section.forEach((i) => {
@@ -9,7 +44,7 @@ window.onscroll = () => {
     let id = i.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      menu.forEach((link) => {
+      nav.forEach((link) => {
         link.classList.remove("active");
         document
           .querySelector("nav li a[href*=" + id + "]")
